7c4b9ce701288a4d4b0245c228e6a72fc46c3aeb
jorgesr92 <jorgesr92@github.com>
2020-11-01 18:18:13 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from "react";
+import Logo from "./components/Logo";
+import BestPokemon from "./components/BestPokemon";
+import CaughtPokemon from "./components/CaughtPokemon";
+import DivHeader from "./components/DivHeader";
 
 
-function Welcome() {
-  return <h1>Welcome to the Pokedex</h1>;
-}
-
-function WelcomeImg() {
-  return <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"></img>;
-}
-
-function DivHeader() {
-  return (
-    <div id="headerDiv">
-      <Welcome />
-      <WelcomeImg />
-    </div>
-  );
-}
-
-function App(){
-  return (
-    <header>
-      <DivHeader />
-    </header>
-  )
-}
+const App = () => (
+  <header>
+    <DivHeader />
+    <Logo />
+    <BestPokemon />
+    <CaughtPokemon />
+  </header>
+);
 
 
 export default App;
